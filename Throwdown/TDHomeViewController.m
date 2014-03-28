7d63eb074a07fa07d3a34fec6383300daa6c1934
Andrew Bennett <github1@explorefun.com>
2014-03-28 09:19:04 -0700

    - Changed order of log out and send feedback buttons

--- Throwdown/TDHomeViewController.m
+++ Throwdown/TDHomeViewController.m
@@ -560,8 +560,8 @@
     UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@""
                                                              delegate:self
                                                     cancelButtonTitle:@"Cancel"
-                                               destructiveButtonTitle:@"Log Out"
-                                                    otherButtonTitles:@"Send Feedback", nil];
+                                               destructiveButtonTitle:@"Send Feedback"
+                                                    otherButtonTitles:@"Log Out", nil];
     actionSheet.tag = 3546;
     [actionSheet showInView:self.view];
 }
@@ -574,12 +574,12 @@
 
     if (actionSheet.tag == 3546) {
 
-        if (buttonIndex == 1)   // Feedback
+        if (buttonIndex == 0)   // Feedback
         {
             [self displayFeedbackEmail];
         }
 
-        if (buttonIndex == 0)   // Log out
+        if (buttonIndex == 1)   // Log out
         {
             [[TDUserAPI sharedInstance] logout];
             [self showWelcomeController];
