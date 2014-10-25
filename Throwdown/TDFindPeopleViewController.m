1c82ce5164ca538aad16f8d8db2905bb2b8819b0
Stephanie Le <stephanie.le@gmail.com>
2014-10-24 17:38:10 -0700

    Throwdown/TDFindPeopleViewController.m

--- Throwdown/TDFindPeopleViewController.m
+++ Throwdown/TDFindPeopleViewController.m
@@ -352,9 +352,11 @@
         cell.delegate = self;
     }
     
-    cell.topLine.hidden = NO;
+
     cell.userId = [object valueForKey:@"id"];
     cell.row = indexPath.row;
+    cell.topLine.hidden = cell.row != 0;
+
     NSAttributedString *usernameAttStr = nil;
     NSString *str = [NSString stringWithFormat:@"%@", [object valueForKey:@"bio"]];
     if (str == nil || str.length == 0 || [[object valueForKey:@"bio"] isKindOfClass:[NSNull class]]) {
